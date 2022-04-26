c7dbc3325db6d68c2240b42f5a41f31040275fb9
matildasutho <64000876+matildasutho@users.noreply.github.com>
2022-04-26 10:41:37 +1000

    Add files via upload

---
+++ DRAFT/canvases.js
@@ -0,0 +1,237 @@
+// kae tempest quote 
+var canvas = document.getElementById("tempest");
+        var ctx = canvas.getContext("2d");
+        window.devicePixelRatio=2;  
+        var size = 400;
+        canvas.style.width = size + "px";
+        canvas.style.height = size + "px";
+
+        var scale = window.devicePixelRatio; 
+        canvas.width = Math.floor(size * scale);
+        canvas.height = Math.floor(size * scale);
+        ctx.rotate(0 * Math.PI / 180);
+
+        ctx.font = "4rem times";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "left";
+        ctx.fillText("stop suffering", 200, 100);
+
+        ctx.font = "4rem times";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "left";
+        ctx.fillText("dance more", 200, 145);
+
+        ctx.font = "2rem times";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "right";
+        ctx.fillText("Kae Tempest", 700, 230);
+
+// canvas XXX with COWGIRLCOWGIRL
+var canvas = document.getElementById("cowgXrl");
+        var ctx2 = canvas.getContext("2d");
+        window.devicePixelRatio=2;  
+        var size = 900;
+        canvas.style.width = size + "px";
+        canvas.style.height = size + "px";
+
+        var scale = window.devicePixelRatio; 
+        canvas.width = Math.floor(size * scale);
+        canvas.height = Math.floor(size * scale);
+        ctx2.rotate(0 * Math.PI / 180);
+
+        ctx2.font = "bold 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("COWGIRL", 200, 200);
+
+        ctx2.font = "bold 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("COWGIRL", 475, 200);
+        
+        ctx2.font = "bold 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("COWGIRL", 750, 200);
+//LINE 2
+        ctx2.font = "bold 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CXWGIRL", 200, 250);
+
+        ctx2.font = "bold 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CXWGIRL", 475, 250);
+        
+        ctx2.font = "bold 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CXWGIRL", 750, 250);
+//LINE 3
+        ctx2.font = "bold 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CXWG", 200, 300);
+
+        ctx2.font = "bold 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CXWG", 475, 300);
+
+        ctx2.font = "bold 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CXW      G", 750, 300);
+
+//LINE 4
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CX W", 200, 350);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CX     W", 475, 350);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CX         W", 750, 350);
+//LINE 5
+        ctx2.font = "italic 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("cxwgxrl", 200, 400);
+
+        ctx2.font = "italic 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("cxwgxrl", 475, 400);
+
+        ctx2.font = "italic 4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("cxwgxrl", 750, 400);
+//LINE 6
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CXXGXRL", 200, 450);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CXXGXRL", 475, 450);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("CXXGXRL", 750, 450);
+//LINE 7
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c   o w g irl", 200, 550);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c   o w g irl", 475, 550);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c   o w g irl", 750, 550);
+//LINE 8
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c   o w g irl", 200, 600);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c   o w g irl", 475, 600);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c   o w gir l", 750, 600);
+//LINE 9
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c o wg  ir  l", 200, 650);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c o wg  ir  l", 475, 650);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c o wg  ir  l", 750, 650);
+ //LINE 9
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c o wg ir   l", 200, 700);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c o wg ir   l", 475, 700);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c o wg ir   l", 750, 700);
+//LINE 10
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c owgir     l", 200, 750);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c owgir     l", 475, 750);
+
+        ctx2.font = "4em arial narrow";
+        ctx2.fillStyle = "white";
+        ctx2.textAlign = "left";
+        ctx2.fillText("c owgir     l", 750, 750);
+//FALLING COWGIRL
+
+var ctx22 =  canvas.getContext("2d");
+        ctx22.rotate(30 * Math.PI / 180);
+        ctx22.font = "4em arial narrow";
+        ctx22.fillStyle = "white";
+        ctx22.textAlign = "left";
+        ctx22.fillText("c", 600, 600);
+
+var ctx23 =  canvas.getContext("2d");
+        ctx23.rotate(30 * Math.PI / 180);
+        ctx23.font = "4em arial narrow";
+        ctx23.fillStyle = "white";
+        ctx23.textAlign = "left";
+        ctx23.fillText("o", 1000, 50);
+
+var ctx24 =  canvas.getContext("2d");
+        ctx24.rotate(-38 * Math.PI / 180);
+        ctx24.font = "4em arial narrow";
+        ctx24.fillStyle = "white";
+        ctx24.textAlign = "left";
+        ctx24.fillText("wg", 1111, 800);
+
+var ctx25 =  canvas.getContext("2d");
+        ctx25.rotate(-20 * Math.PI / 180);
+        ctx25.font = "bold 4em arial narrow";
+        ctx25.fillStyle = "white";
+        ctx25.textAlign = "left";
+        ctx25.fillText("IRL", 950, 1400);
+
+ 
