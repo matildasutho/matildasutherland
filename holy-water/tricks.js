49f3f394051fc6d42eb90dbd63e0efd1645fdf93
matildasutho <64000876+matildasutho@users.noreply.github.com>
2022-06-24 14:48:00 +1000

    Add files via upload

---
+++ holy-water/tricks.js
@@ -0,0 +1,17 @@
+function textReveal() {
+    document.getElementById("").style.fill = "rgba(255, 217, 217, 0.85)";
+}
+
+function textHide() {
+    document.getElementById("").style.fill = "rgba(255, 217, 217, 0)";
+};
+
+function textReveal() {
+    document.getElementById("").style.fill = "rgba(255, 217, 217, 0.85)";
+}
+
+function textHide() {
+    document.getElementById("").style.fill = "rgba(255, 217, 217, 0)";
+};
+
+var first = document.getElementById("")
