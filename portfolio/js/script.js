3b7207b1bc2d71798c2ff2e3735074afc0f80ef9
Matilda Sutherland <64000876+matildasutho@users.noreply.github.com>
2023-04-04 12:59:06 +0200

    Add files via upload

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,35 +1,53 @@
-$(document).ready(function(){
-  $(".radial-one").click(function(){
+$(document).ready(function () {
+  $(".radial-one").click(function () {
     $("#aboot").toggleClass("hidden");
   });
-  $(".radial-four").click(function(){
-    $("#contact-open").toggleClass("hidden");
-  });
-});
-$(document).ready(function(){
-
-});
-$("#projects-btn").click(function() {
-  $('html,body').animate({
-      scrollTop: $("#projects-page").offset().top},
-      'fast');
 });
 
-$("#title-head").click(function() {
-  $('html,body').animate({
-      scrollTop: $("#first-cont").offset().top},
-      'fast');
+$("#about-btn").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#about-page").offset().top,
+    },
+    "fast"
+  );
 });
-$("#skills-btn").click(function() {
-  $('html,body').animate({
-      scrollTop: $("#skills-page").offset().top},
-      'fast');
+$("#title-head").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#first-cont").offset().top,
+    },
+    "fast"
+  );
+});
+$("#projects-btn").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#projects-page").offset().top,
+    },
+    "fast"
+  );
+});
+$("#skills-btn").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#skills-page").offset().top,
+    },
+    "fast"
+  );
+});
+$("#contact-btn").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#contact-page").offset().top,
+    },
+    "fast"
+  );
 });
 // image follow mouse function
-$(document).mousemove(function(e) {
+$(document).mousemove(function (e) {
   $("#follow").css({
     left: e.pageX,
-    top: e.pageY
+    top: e.pageY,
   });
 });
-
