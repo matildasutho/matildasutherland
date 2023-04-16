a0da3e2f162f840f7d90cde0930dc7b30d7214c7
Matilda Sutherland <64000876+matildasutho@users.noreply.github.com>
2023-04-16 17:36:56 +0200

    Add files via upload

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,11 +1,4 @@
-$("#about-btn").click(function () {
-  $("html,body").animate(
-    {
-      scrollTop: $("#about-page").offset().top,
-    },
-    "fast"
-  );
-});
+
 $("#title-head").click(function () {
   $("html,body").animate(
     {
@@ -14,6 +7,15 @@ $("#title-head").click(function () {
     "fast"
   );
 });
+$("#about-btn").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#about-page").offset().top,
+    },
+    "fast"
+  );
+});
+
 $("#projects-btn").click(function () {
   $("html,body").animate(
     {
@@ -33,4 +35,29 @@ $("#contact-btn").click(function () {
       scrollTop: $("#contact-page").offset().top - 48},
     "fast"
   );
+});
+
+$("#btn-1").on('click', function () {
+  $("#img-box-1").show(1000);
+});
+$("#img-box-1").on("click", function () {
+  $("#img-box-1").hide(1000);
+});
+$("#btn-2").on("click", function () {
+  $("#img-box-2").show(1000);
+});
+$("#img-box-2").on("click", function () {
+  $("#img-box-2").hide(1000);
+});
+$("#btn-3").on("click", function () {
+  $("#img-box-3").show(1000);
+});
+$("#img-box-3").on("click", function () {
+  $("#img-box-3").hide(1000);
+});
+$("#btn-4").on("click", function () {
+  $("#img-box-4").show(1000);
+});
+$("#img-box-4").on("click", function () {
+  $("#img-box-4").hide(1000);
 });
